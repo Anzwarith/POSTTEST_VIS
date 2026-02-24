2a0366e4a468db40d2b86b7c58e1cf4c9a7dbbf5
Anzwarith <hazzel0709@gmail.com>
2026-02-24 23:29:10 +0800

    Add project files.

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblIPS = New Label()
+        lblIPK = New Label()
+        lblPredikat = New Label()
+        txtIPS = New TextBox()
+        txtIPK = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        txtPredikat = New TextBox()
+        SuspendLayout()
+        ' 
+        ' lblIPS
+        ' 
+        lblIPS.AutoSize = True
+        lblIPS.BackColor = Color.Transparent
+        lblIPS.Location = New Point(290, 246)
+        lblIPS.Name = "lblIPS"
+        lblIPS.Size = New Size(86, 20)
+        lblIPS.TabIndex = 0
+        lblIPS.Text = "IP Semester"
+        ' 
+        ' lblIPK
+        ' 
+        lblIPK.AutoSize = True
+        lblIPK.Location = New Point(444, 245)
+        lblIPK.Name = "lblIPK"
+        lblIPK.Size = New Size(89, 20)
+        lblIPK.TabIndex = 1
+        lblIPK.Text = "IP Kumulatif"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.Anchor = AnchorStyles.Top
+        lblPredikat.AutoSize = True
+        lblPredikat.FlatStyle = FlatStyle.Popup
+        lblPredikat.Location = New Point(381, 146)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(63, 20)
+        lblPredikat.TabIndex = 2
+        lblPredikat.Text = "Predikat"
+        lblPredikat.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.Location = New Point(272, 216)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(125, 27)
+        txtIPS.TabIndex = 3
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.Location = New Point(425, 215)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(125, 27)
+        txtIPK.TabIndex = 4
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.BackColor = Color.DodgerBlue
+        btnTambah.BackgroundImageLayout = ImageLayout.None
+        btnTambah.FlatStyle = FlatStyle.Popup
+        btnTambah.ForeColor = SystemColors.ButtonHighlight
+        btnTambah.Location = New Point(172, 216)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 5
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.BackColor = Color.LightBlue
+        btnReset.FlatStyle = FlatStyle.Flat
+        btnReset.Location = New Point(556, 214)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 6
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = False
+        ' 
+        ' txtPredikat
+        ' 
+        txtPredikat.Anchor = AnchorStyles.Top
+        txtPredikat.BackColor = SystemColors.ButtonHighlight
+        txtPredikat.Location = New Point(283, 169)
+        txtPredikat.Name = "txtPredikat"
+        txtPredikat.ReadOnly = True
+        txtPredikat.Size = New Size(250, 27)
+        txtPredikat.TabIndex = 7
+        txtPredikat.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.CornflowerBlue
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtPredikat)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPK)
+        Controls.Add(txtIPS)
+        Controls.Add(lblPredikat)
+        Controls.Add(lblIPK)
+        Controls.Add(lblIPS)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblIPS As Label
+    Friend WithEvents lblIPK As Label
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtPredikat As TextBox
+
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    Dim totalIPS As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        If txtIPS.Text = "" Then
+            MessageBox.Show("Masukkan IP Semester dulu.")
+            Exit Sub
+        End If
+
+        Dim ips As Double
+        If Not Double.TryParse(txtIPS.Text, ips) Then
+            MessageBox.Show("Input harus angka.")
+            Exit Sub
+        End If
+
+        ' Tambah data
+        totalIPS += ips
+        jumlahSemester += 1
+
+        ' Hitung IPK
+        Dim ipk As Double = totalIPS / jumlahSemester
+        txtIPK.Text = ipk.ToString("0.00")
+
+        ' ===== INI BAGIAN PREDIKAT =====
+        txtPredikat.Text = HitungPredikat(ipk)
+
+        ' Kosongkan IPS
+        txtIPS.Clear()
+        txtIPS.Focus()
+
+    End Sub
+
+    Private Function HitungPredikat(ipk As Double) As String
+        If ipk >= 2.0 And ipk <= 2.75 Then
+            Return "Cukup"
+        ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+            Return "Memuaskan"
+        ElseIf ipk >= 3.01 Then
+            Return "Sangat Memuaskan"
+        Else
+            Return "-"
+        End If
+    End Function
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIPS = 0
+        jumlahSemester = 0
+
+        txtIPS.Clear()
+        txtIPK.Clear()
+        lblPredikat.Text = "-"
+
+        txtIPS.Focus()
+    End Sub
+End Class
